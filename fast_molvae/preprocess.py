eb35c73458dff160b2319b73a5aa8fdc1a8b3783
barry_Cin <barry@cinnamon.is>
2019-06-05 09:21:32 +0700

    Update code

--- fast_molvae/preprocess.py
+++ fast_molvae/preprocess.py
@@ -4,7 +4,10 @@ from multiprocessing import Pool
 
 import math, random, sys
 from optparse import OptionParser
-import cPickle as pickle
+import pickle
+
+import sys
+sys.path.append('D:\\workspace\\python\\vae-jt')
 
 from fast_jtnn import *
 import rdkit
@@ -45,7 +48,7 @@ if __name__ == "__main__":
 
     le = (len(all_data) + num_splits - 1) / num_splits
 
-    for split_id in xrange(num_splits):
+    for split_id in range(num_splits):
         st = split_id * le
         sub_data = all_data[st : st + le]
 
